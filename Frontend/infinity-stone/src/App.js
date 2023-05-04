a5b2f863af51d984634bd013a6300e01d6860264
VivekTomar03 <tomarvivek90viv@gmail.com>
2023-05-04 14:18:43 +0530

    day1

--- Frontend/infinity-stone/src/App.js
+++ Frontend/infinity-stone/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AdminMain from './Pages/Admin/AdminMain';
 
 function App() {
   return (
     <div className="App">
-      
+      <AdminMain/>
     </div>
   );
 }
