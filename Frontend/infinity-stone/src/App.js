b9ff24a030f81adb68a0cb167d70c20045e2eed7
sumit <sumitkolipyaka99@gmail.com>
2023-05-04 21:30:21 +0530

    Done with all the Issues and Tasks of Day 2

--- Frontend/infinity-stone/src/App.js
+++ Frontend/infinity-stone/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
+import Ring from './Pages/products pages/Ring';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+
       
     </div>
   );
