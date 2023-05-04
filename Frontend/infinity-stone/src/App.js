8f01b880c0e67c58a34bcc711cbe9fb4699ed148
MrunaliGabhane <mrunagabhane@gmail.com>
2023-05-04 21:47:19 +0530

    fw21_0863

--- Frontend/infinity-stone/src/App.js
+++ Frontend/infinity-stone/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+// import EmptyCart from './Components/EmptyCart/EmptyCard';
+// import Cartmap from './Components/CartMap/Cartmap';
 
 function App() {
   return (
     <div className="App">
-      
+      {/* <EmptyCart/> */}
+      {/* <Cartmap/> */}
     </div>
   );
 }
