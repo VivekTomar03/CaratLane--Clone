6bdba2aaf03735f7ba86ac39088b7d284ff48169
MrunaliGabhane <mrunagabhane@gmail.com>
2023-05-05 18:44:24 +0530

    paymentPage_UI

--- Frontend/infinity-stone/src/App.js
+++ Frontend/infinity-stone/src/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Cart from './Pages/Cartpage/Cart';
-// import EmptyCart from './Components/EmptyCart/EmptyCard';
-// import Cartmap from './Components/CartMap/Cartmap';
+import Payment from './Pages/Payment/Payment';
+
 
 function App() {
   return (
     <div className="App">
-      {/* <EmptyCart/> */}
-      {/* <Cartmap/> */}
-      <Cart/>
+     
     </div>
   );
 }
